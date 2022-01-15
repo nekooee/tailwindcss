1319496f8385defdb06583e783d275458ec55885
armin nekooee <nekooee@gmail.com>
2022-01-15 18:33:56 +0330

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<h1 class="text-3xl font-bold text-red-700 dark:text-blue-700">Hello world!</h1>
+</body>
+</html>
